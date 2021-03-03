d961c499d840f2dbc2fb39458f6adbaa47e31681
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2021-03-03 19:36:19 +0000

    Kall á rest komið en utanumhald á innskráðan notanda vantar

--- app/src/main/java/is/hi/hbv601g/matbjorg_app/network/NetworkController.java
+++ app/src/main/java/is/hi/hbv601g/matbjorg_app/network/NetworkController.java
@@ -7,23 +7,27 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import is.hi.hbv601g.matbjorg_app.models.Buyer;
 import is.hi.hbv601g.matbjorg_app.models.Seller;
+import is.hi.hbv601g.matbjorg_app.models.User;
 
 public class NetworkController {
     private static final String TAG = "NetworkController";
     public static final String MATBJORG_URL_REST = "https://matbjorg.herokuapp.com/rest/";
-    public static final String LOCAL_URL_REST = "http://localhost:8080/rest/";
 
     private Context context;
 
@@ -45,7 +49,7 @@ public class NetworkController {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d(TAG, error.toString());
+                Log.e(TAG, error.toString());
                 networkCallback.onError("Gekk ekki að sækja gögn");
             }
         });
@@ -65,8 +69,28 @@ public class NetworkController {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d(TAG, error.toString());
+                Log.e(TAG, error.toString());
                 networkCallback.onError("Gekk ekki að sækja gögn");
+            }
+        });
+        NetworkSingleton.getInstance(context).addToRequestQueue(request);
+    }
+
+    public void login(NetworkCallback<User> networkCallback, String email, String password) {
+        String url = MATBJORG_URL_REST + String.format("login?email=%s&password=%s", email, password);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+                Gson gson = new Gson();
+                User user = gson.fromJson(response.toString(), new TypeToken<User>(){}.getType());
+                networkCallback.onResponse(user);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.toString());
+                networkCallback.onError("Gekk ekki að skrá notanda inn");
             }
         });
         NetworkSingleton.getInstance(context).addToRequestQueue(request);
--- app/src/main/java/is/hi/hbv601g/matbjorg_app/ui/LoginActivity.java
+++ app/src/main/java/is/hi/hbv601g/matbjorg_app/ui/LoginActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 import is.hi.hbv601g.matbjorg_app.R;
+import is.hi.hbv601g.matbjorg_app.models.Seller;
+import is.hi.hbv601g.matbjorg_app.models.User;
+import is.hi.hbv601g.matbjorg_app.network.NetworkCallback;
+import is.hi.hbv601g.matbjorg_app.network.NetworkController;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText mEmail;
+    private EditText mPassword;
     private Button mLogin;
 
     public static Intent newIntent(Context packageContext) {
@@ -23,11 +34,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        NetworkController networkController = new NetworkController(LoginActivity.this);
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
         mLogin = (Button) findViewById(R.id.login);
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Login virkni
+                Toast.makeText(LoginActivity.this, "Username: " + mEmail.getText().toString() + "\n Password: " + mPassword.getText().toString(), Toast.LENGTH_SHORT).show();
+                networkController.login(new NetworkCallback<User>() {
+                    @Override
+                    public void onError(String error) {
+                        Toast.makeText(LoginActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(User user) {
+                        SharedPreferences sharedPref = LoginActivity.this.getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString("loggedin_user_id", ""+user.getId());
+                        editor.apply();
+                        // TODO: Fæ ekki til að virka
+                        Toast.makeText(LoginActivity.this, "Innskráning gekk", Toast.LENGTH_SHORT).show();
+                    }
+                }, mEmail.getText().toString(), mPassword.getText().toString());
             }
         });
     }
