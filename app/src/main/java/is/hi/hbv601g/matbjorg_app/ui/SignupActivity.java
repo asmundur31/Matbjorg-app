45ebe665219e46f91d6fa0823cb0fa195bbcf917
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2021-03-03 19:45:45 +0000

    Byrjaður á signup virkni

---
+++ app/src/main/java/is/hi/hbv601g/matbjorg_app/ui/SignupActivity.java
@@ -0,0 +1,16 @@
+package is.hi.hbv601g.matbjorg_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import is.hi.hbv601g.matbjorg_app.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
