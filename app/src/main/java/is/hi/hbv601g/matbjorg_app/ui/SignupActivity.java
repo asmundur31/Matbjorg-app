3f50f9bb0739e9678c3b37ba7d6c74551d3c0c0a
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2021-04-01 13:26:14 +0000

    lagaði signup

--- app/src/main/java/is/hi/hbv601g/matbjorg_app/ui/SignupActivity.java
+++ app/src/main/java/is/hi/hbv601g/matbjorg_app/ui/SignupActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,10 +26,14 @@ public class SignupActivity extends AppCompatActivity {
 
     private Button mSignup;
     private EditText mName;
+    private boolean isName;
     private EditText mEmail;
+    private boolean isEmail;
     private EditText mPassword;
+    private boolean isPassword;
     private RadioGroup mRadioGroup;
     private RadioButton mRadioButton;
+    private boolean isType;
 
     public static Intent newIntent(Context packageContext) {
         Intent intent = new Intent(packageContext, SignupActivity.class);
@@ -39,20 +45,27 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         NetworkController networkController = new NetworkController(SignupActivity.this);
+        mName = (EditText) findViewById(R.id.signup_name);
         mEmail = (EditText) findViewById(R.id.signup_username);
         mPassword = (EditText) findViewById(R.id.signup_password);
-        mName = (EditText) findViewById(R.id.signup_name);
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         mSignup = (Button) findViewById(R.id.signup);
+
+        // Hlustarar til að passa að Nýskrá takki sé óvirkur ef það vantar upplýsingar
+        addNameTextChangeListener();
+        addEmailTextChangeListener();
+        addPasswordTextChangeListener();
+        addTypeListener();
+
         mSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
                 int selectedId = mRadioGroup.getCheckedRadioButtonId();
                 mRadioButton = (RadioButton) findViewById(selectedId);
                 networkController.signup(new NetworkCallback<User>() {
                     @Override
                     public void onError(String error) {
-                        Toast.makeText(SignupActivity.this, error, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SignupActivity.this, error + "\nNetfang frátekið eða á vitlausu formi", Toast.LENGTH_LONG).show();
                     }
 
                     @Override
@@ -64,9 +77,93 @@ public class SignupActivity extends AppCompatActivity {
                         editor.putString("token", user.getToken());
                         editor.apply();
                         Toast.makeText(SignupActivity.this, "Nýskráning gekk", Toast.LENGTH_SHORT).show();
+                        finish();
                     }
                 }, mRadioButton.getText().toString(), mName.getText().toString(), mEmail.getText().toString(), mPassword.getText().toString());
             }
         });
+    }
+
+    private void addNameTextChangeListener() {
+        mName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() > 0) {
+                    isName = true;
+                } else {
+                    isName = false;
+                }
+                checkEverything();
+            }
+        });
+    }
+
+    private void addEmailTextChangeListener() {
+        mEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() > 0) {
+                    isEmail = true;
+                } else {
+                    isEmail = false;
+                }
+                checkEverything();
+            }
+        });
+    }
+
+    private void addPasswordTextChangeListener() {
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() > 0) {
+                    isPassword = true;
+                } else {
+                    isPassword = false;
+                }
+                checkEverything();
+            }
+        });
+    }
+
+    private void addTypeListener() {
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int selectedId = mRadioGroup.getCheckedRadioButtonId();
+                if (selectedId == -1) {
+                    isType = false;
+                } else {
+                    isType = true;
+                }
+                checkEverything();
+            }
+        });
+    }
+
+    private void checkEverything() {
+        if (isName && isEmail && isPassword && isType) {
+            mSignup.setEnabled(true);
+        } else {
+            mSignup.setEnabled(false);
+        }
     }
 }
